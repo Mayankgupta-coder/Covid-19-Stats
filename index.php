47ed73f6896c238e2fa0e2974f37121324ed3540
Mayankgupta-coder <mynkgpt16@gmail.com>
2021-06-19 00:23:13 +0530

    Numbe system improved

--- index.php
+++ index.php
@@ -4,6 +4,92 @@ require('header.php');
 $content=file_get_contents('https://api.covid19india.org/data.json');
 $content_arr=json_decode($content,true);
 $len=count($content_arr['cases_time_series']);
+function money_format($format, $number)
+{
+    $regex  = '/%((?:[\^!\-]|\+|\(|\=.)*)([0-9]+)?'.
+              '(?:#([0-9]+))?(?:\.([0-9]+))?([in%])/';
+    if (setlocale(LC_MONETARY, 0) == 'C') {
+        setlocale(LC_MONETARY, '');
+    }
+    $locale = localeconv();
+    preg_match_all($regex, $format, $matches, PREG_SET_ORDER);
+    foreach ($matches as $fmatch) {
+        $value = floatval($number);
+        $flags = array(
+            'fillchar'  => preg_match('/\=(.)/', $fmatch[1], $match) ?
+                           $match[1] : ' ',
+            'nogroup'   => preg_match('/\^/', $fmatch[1]) > 0,
+            'usesignal' => preg_match('/\+|\(/', $fmatch[1], $match) ?
+                           $match[0] : '+',
+            'nosimbol'  => preg_match('/\!/', $fmatch[1]) > 0,
+            'isleft'    => preg_match('/\-/', $fmatch[1]) > 0
+        );
+        $width      = trim($fmatch[2]) ? (int)$fmatch[2] : 0;
+        $left       = trim($fmatch[3]) ? (int)$fmatch[3] : 0;
+        $right      = trim($fmatch[4]) ? (int)$fmatch[4] : $locale['int_frac_digits'];
+        $conversion = $fmatch[5];
+
+        $positive = true;
+        if ($value < 0) {
+            $positive = false;
+            $value  *= -1;
+        }
+        $letter = $positive ? 'p' : 'n';
+
+        $prefix = $suffix = $cprefix = $csuffix = $signal = '';
+
+        $signal = $positive ? $locale['positive_sign'] : $locale['negative_sign'];
+        switch (true) {
+            case $locale["{$letter}_sign_posn"] == 1 && $flags['usesignal'] == '+':
+                $prefix = $signal;
+                break;
+            case $locale["{$letter}_sign_posn"] == 2 && $flags['usesignal'] == '+':
+                $suffix = $signal;
+                break;
+            case $locale["{$letter}_sign_posn"] == 3 && $flags['usesignal'] == '+':
+                $cprefix = $signal;
+                break;
+            case $locale["{$letter}_sign_posn"] == 4 && $flags['usesignal'] == '+':
+                $csuffix = $signal;
+                break;
+            case $flags['usesignal'] == '(':
+            case $locale["{$letter}_sign_posn"] == 0:
+                $prefix = '(';
+                $suffix = ')';
+                break;
+        }
+        if (!$flags['nosimbol']) {
+            $currency = $cprefix .
+                        ($conversion == 'i' ? $locale['int_curr_symbol'] : $locale['currency_symbol']) .
+                        $csuffix;
+        } else {
+            $currency = '';
+        }
+        $space  = $locale["{$letter}_sep_by_space"] ? ' ' : '';
+
+        $value = number_format($value, $right, $locale['mon_decimal_point'],
+                 $flags['nogroup'] ? '' : $locale['mon_thousands_sep']);
+        $value = @explode($locale['mon_decimal_point'], $value);
+
+        $n = strlen($prefix) + strlen($currency) + strlen($value[0]);
+        if ($left > 0 && $left > $n) {
+            $value[0] = str_repeat($flags['fillchar'], $left - $n) . $value[0];
+        }
+        $value = implode($locale['mon_decimal_point'], $value);
+        if ($locale["{$letter}_cs_precedes"]) {
+            $value = $prefix . $currency . $space . $value . $suffix;
+        } else {
+            $value = $prefix . $value . $space . $currency . $suffix;
+        }
+        if ($width > 0) {
+            $value = str_pad($value, $width, $flags['fillchar'], $flags['isleft'] ?
+                     STR_PAD_RIGHT : STR_PAD_LEFT);
+        }
+
+        $format = str_replace($fmatch[0], $value, $format);
+    }
+    return explode(".",$format);
+}
 ?>
 
 <html>
@@ -153,6 +239,22 @@ google.charts.load('current', {'packages':['corechart']});
         
 </head>
 <body>
+  <?php
+$confirm = $content_arr['cases_time_series'][$len-1]['totalconfirmed'];
+setlocale(LC_MONETARY, 'en_IN');
+$confirm = money_format('%!i', $confirm);
+
+
+$recover= $content_arr['cases_time_series'][$len-1]['totalrecovered'];
+setlocale(LC_MONETARY, 'en_IN');
+$recover = money_format('%!i', $recover);
+
+
+$deceased = $content_arr['cases_time_series'][$len-1]['totaldeceased'];
+setlocale(LC_MONETARY, 'en_IN');
+$deceased = money_format('%!i', $deceased);
+
+?>
 <br/>
 <div class="text">Covid-19 Data</div>
 <br/>
@@ -161,19 +263,19 @@ google.charts.load('current', {'packages':['corechart']});
 <b>CONFIRMED CASES</b>
 <br/>
 <br/>
-<b><?php echo $content_arr['cases_time_series'][$len-1]['totalconfirmed']?></b>
+<b><?php echo $confirm[0] ?></b>
 </div>
 <div class="container-fluid">
 <b>RECOVERED CASES</b>
 <br/>
 <br/>
-<b><?php echo $content_arr['cases_time_series'][$len-1]['totalrecovered']?></b>
+<b><?php echo $recover[0] ?></b>
 </div>
 <div class="container-fluid">
 <b>DECEASED CASES</b>
 <br/>
 <br/>
-<b><?php echo $content_arr['cases_time_series'][$len-1]['totaldeceased']?></b>
+<b><?php echo $deceased[0]?></b>
 </div>
     </div>
     <hr>

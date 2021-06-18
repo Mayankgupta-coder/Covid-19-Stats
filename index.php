95fbc99c8d838434d843d695c34fbdb98192780f
Mayankgupta-coder <mynkgpt16@gmail.com>
2021-06-18 21:53:24 +0530

    First commit

---
+++ index.php
@@ -0,0 +1,178 @@
+<?php
+$content=file_get_contents('https://api.covid19india.org/data.json');
+$content_arr=json_decode($content,true);
+$len=count($content_arr['cases_time_series']);
+?>
+
+<html>
+    <head>
+    <script type="text/javascript" src="https://www.gstatic.com/charts/loader.js"></script>
+    <script type="text/javascript">
+
+google.charts.load('current', {'packages':['corechart']});
+      google.charts.setOnLoadCallback(drawChart);
+
+      function drawChart() {
+        var data = google.visualization.arrayToDataTable([
+          ['Cases', ''],
+          <?php for($i=12;$i>=1;$i--)
+          {
+              $date=$content_arr['cases_time_series'][$len-$i]['date'];
+              ?>
+          ['<?php echo $date?>',  <?php echo $content_arr['cases_time_series'][$len-$i]['dailyconfirmed']?>],
+          
+          <?php
+          }
+          ?>
+        ]);
+
+        var options = {
+          
+          curveType: 'function',
+          legend: { position: 'bottom' }
+        };
+
+        var chart = new google.visualization.LineChart(document.getElementById('curve_chart'));
+
+        chart.draw(data, options);
+        
+      }
+
+
+
+      google.charts.load('current', {'packages':['corechart']});
+      google.charts.setOnLoadCallback(drawChart1);
+
+      function drawChart1() {
+        var data1 = google.visualization.arrayToDataTable([
+          ['Cases', ''],
+          <?php for($i=12;$i>=1;$i--)
+          {
+              $date=$content_arr['cases_time_series'][$len-$i]['date'];
+              ?>
+          ['<?php echo $date?>',  <?php echo $content_arr['cases_time_series'][$len-$i]['dailyrecovered']?>],
+          
+          <?php
+          }
+          ?>
+        ]);
+
+        var options1 = {
+          
+          curveType: 'function',
+          legend: { position: 'bottom' }
+        };
+
+        var chart1 = new google.visualization.LineChart(document.getElementById('curve_chart1'));
+
+        chart1.draw(data1, options1);
+        
+      }
+
+
+      google.charts.load('current', {'packages':['corechart']});
+      google.charts.setOnLoadCallback(drawChart2);
+
+      function drawChart2() {
+        var data2 = google.visualization.arrayToDataTable([
+          ['Cases', ''],
+          <?php for($i=12;$i>=1;$i--)
+          {
+              $date=$content_arr['cases_time_series'][$len-$i]['date'];
+              ?>
+          ['<?php echo $date?>',  <?php echo $content_arr['cases_time_series'][$len-$i]['dailydeceased']?>],
+          
+          <?php
+          }
+          ?>
+        ]);
+
+        var options2= {
+          
+          curveType: 'function',
+          legend: { position: 'bottom' }
+        };
+
+        var chart2 = new google.visualization.LineChart(document.getElementById('curve_chart2'));
+
+        chart2.draw(data2, options2);
+        
+      }
+</script>
+
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-+0n0xVW2eSR5OomGNYDnhzAbDsOXxcvSN1TPprVMTNDbiYZCxYbOOl7+AMvyTG2x" crossorigin="anonymous">
+    <style>
+        #div2
+        {
+            background-image:url('images/recovered_cases.jpg');
+        }
+        p
+        {
+            text-align:center;
+        }
+        .container-fluid
+        {
+            height:200px;
+            width:300px;
+            border:1px solid black;
+            box-shadow: 3px 3px grey;
+            margin-bottom:8px;
+            text-align:Center;
+            padding:40px 0px;
+        }
+        #div
+        {
+            display:flex;
+            flex-wrap:wrap;
+        }
+      .chart{
+         width:1300px;
+         height:500px;
+      }
+      .text
+      {
+          text-align:center;
+          color:Red;
+          font-size:40px;
+      }
+        </style>
+        
+</head>
+<body>
+
+<div class="text">Covid-19 Data</div>
+<br/>
+<div id="div">
+<div class="container-fluid">
+<b>CONFIRMED</b>
+<br/>
+<br/>
+<b><?php echo $content_arr['cases_time_series'][$len-1]['totalconfirmed']?></b>
+</div>
+<div class="container-fluid">
+<b>RECOVERED</b>
+<br/>
+<br/>
+<b><?php echo $content_arr['cases_time_series'][$len-1]['totalrecovered']?></b>
+</div>
+<div class="container-fluid">
+<b>DECEASED</b>
+<br/>
+<br/>
+<b><?php echo $content_arr['cases_time_series'][$len-1]['totaldeceased']?></b>
+</div>
+    </div>
+    <hr>
+    <br/>
+    <br/>
+    <div class="text">Statistical Analysis</div>
+    <div id="curve_chart" class="chart" ></div>
+    <p> Daily Confirmed Cases</P>
+    <div id="curve_chart1" class="chart" ></div>
+    </div>
+    <p> Daily Recovered Cases</P>
+    <div id="curve_chart2"  class="chart"></div>
+    </div>
+    <p> Daily Deceased Cases</P>
+</body>
+</html>
---
+++ statewise_data.php
@@ -0,0 +1,51 @@
+<?php
+$content=file_get_contents('https://api.covid19india.org/data.json');
+$content_arr=json_decode($content,true);
+$len=count($content_arr['statewise']);
+?>
+<html>
+<head>
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-+0n0xVW2eSR5OomGNYDnhzAbDsOXxcvSN1TPprVMTNDbiYZCxYbOOl7+AMvyTG2x" crossorigin="anonymous">
+</head>
+<body>
+<table class="table table-striped">
+  <thead>
+    <tr>
+      <th scope="col" width="5%">#</th>
+      <th scope="col" width="30%">State/UT</th>
+      <th scope="col" width="20%">Active</th>
+      <th scope="col" width="20%">Confirmed</th>
+      <th scope="col" width="20%">Death</th>
+      <th scope="col" width="20%">Recovered</th>
+    </tr>
+  </thead>
+  <tbody>
+      <?php 
+      $c=0;
+      for($i=1;$i<$len;$i++)
+      {
+          
+          ?>
+    <tr>
+        <?php 
+        if($content_arr['statewise'][$i]['state']!="State Unassigned")
+        {
+        ?>
+      <td scope="row"><?php echo $c+1?></td>
+      <td width="30%"><?php echo $content_arr['statewise'][$i]['state']?></td>
+      <td width="20%"><?php echo $content_arr['statewise'][$i]['active']?></td>
+      <td width="20%"><?php echo $content_arr['statewise'][$i]['confirmed']?></td>
+      <td width="20%"><?php echo $content_arr['statewise'][$i]['deaths']?></td>
+      <td width="20%"><?php echo $content_arr['statewise'][$i]['recovered']?></td>
+      <?php
+      }
+      ?>
+    </tr>
+    <?php
+    $c++;
+      }
+    ?>
+  </tbody>
+</table>
+</body>
+</html>

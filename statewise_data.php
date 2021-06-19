a82bf3557e73367a5cbc5cf5cd4a89e9fd98adb1
Mayankgupta-coder <mynkgpt16@gmail.com>
2021-06-19 13:13:29 +0530

    Improve code quality

--- statewise_data.php
+++ statewise_data.php
@@ -1,94 +1,9 @@
 <?php
 require('header.php');
+require('function.php');
 $content=file_get_contents('https://api.covid19india.org/data.json');
 $content_arr=json_decode($content,true);
 $len=count($content_arr['statewise']);
-function money_format($format, $number)
-{
-    $regex  = '/%((?:[\^!\-]|\+|\(|\=.)*)([0-9]+)?'.
-              '(?:#([0-9]+))?(?:\.([0-9]+))?([in%])/';
-    if (setlocale(LC_MONETARY, 0) == 'C') {
-        setlocale(LC_MONETARY, '');
-    }
-    $locale = localeconv();
-    preg_match_all($regex, $format, $matches, PREG_SET_ORDER);
-    foreach ($matches as $fmatch) {
-        $value = floatval($number);
-        $flags = array(
-            'fillchar'  => preg_match('/\=(.)/', $fmatch[1], $match) ?
-                           $match[1] : ' ',
-            'nogroup'   => preg_match('/\^/', $fmatch[1]) > 0,
-            'usesignal' => preg_match('/\+|\(/', $fmatch[1], $match) ?
-                           $match[0] : '+',
-            'nosimbol'  => preg_match('/\!/', $fmatch[1]) > 0,
-            'isleft'    => preg_match('/\-/', $fmatch[1]) > 0
-        );
-        $width      = trim($fmatch[2]) ? (int)$fmatch[2] : 0;
-        $left       = trim($fmatch[3]) ? (int)$fmatch[3] : 0;
-        $right      = trim($fmatch[4]) ? (int)$fmatch[4] : $locale['int_frac_digits'];
-        $conversion = $fmatch[5];
-
-        $positive = true;
-        if ($value < 0) {
-            $positive = false;
-            $value  *= -1;
-        }
-        $letter = $positive ? 'p' : 'n';
-
-        $prefix = $suffix = $cprefix = $csuffix = $signal = '';
-
-        $signal = $positive ? $locale['positive_sign'] : $locale['negative_sign'];
-        switch (true) {
-            case $locale["{$letter}_sign_posn"] == 1 && $flags['usesignal'] == '+':
-                $prefix = $signal;
-                break;
-            case $locale["{$letter}_sign_posn"] == 2 && $flags['usesignal'] == '+':
-                $suffix = $signal;
-                break;
-            case $locale["{$letter}_sign_posn"] == 3 && $flags['usesignal'] == '+':
-                $cprefix = $signal;
-                break;
-            case $locale["{$letter}_sign_posn"] == 4 && $flags['usesignal'] == '+':
-                $csuffix = $signal;
-                break;
-            case $flags['usesignal'] == '(':
-            case $locale["{$letter}_sign_posn"] == 0:
-                $prefix = '(';
-                $suffix = ')';
-                break;
-        }
-        if (!$flags['nosimbol']) {
-            $currency = $cprefix .
-                        ($conversion == 'i' ? $locale['int_curr_symbol'] : $locale['currency_symbol']) .
-                        $csuffix;
-        } else {
-            $currency = '';
-        }
-        $space  = $locale["{$letter}_sep_by_space"] ? ' ' : '';
-
-        $value = number_format($value, $right, $locale['mon_decimal_point'],
-                 $flags['nogroup'] ? '' : $locale['mon_thousands_sep']);
-        $value = @explode($locale['mon_decimal_point'], $value);
-
-        $n = strlen($prefix) + strlen($currency) + strlen($value[0]);
-        if ($left > 0 && $left > $n) {
-            $value[0] = str_repeat($flags['fillchar'], $left - $n) . $value[0];
-        }
-        $value = implode($locale['mon_decimal_point'], $value);
-        if ($locale["{$letter}_cs_precedes"]) {
-            $value = $prefix . $currency . $space . $value . $suffix;
-        } else {
-            $value = $prefix . $value . $space . $currency . $suffix;
-        }
-        if ($width > 0) {
-            $value = str_pad($value, $width, $flags['fillchar'], $flags['isleft'] ?
-                     STR_PAD_RIGHT : STR_PAD_LEFT);
-        }
-
-        $format = str_replace($fmatch[0], $value, $format);
-    }
-    return explode(".",$format);
-}
 ?>
 <html>
 <head>
@@ -96,10 +11,10 @@ function money_format($format, $number)
 </head>
 <body>
 <br/><br/>
+<div class="table-responsive-lg">
 <table class="table table-striped">
   <thead>
     <tr>
-      <th scope="col" width="5%">#</th>
       <th scope="col" width="30%">State/UT</th>
       <th scope="col" width="20%">Active</th>
       <th scope="col" width="20%">Confirmed</th>
@@ -139,7 +54,7 @@ function money_format($format, $number)
           setlocale(LC_MONETARY, 'en_IN');
           $deceased = money_format('%!i', $deceased);
         ?>
-      <td scope="row"><?php echo $c+1?></td>
+      
       <td width="30%"><?php echo $content_arr['statewise'][$i]['state']?></td>
       <td width="20%"><?php echo $active[0]?></td>
       <td width="20%"><?php echo $confirm[0]?></td>
@@ -156,5 +71,6 @@ function money_format($format, $number)
     ?>
   </tbody>
 </table>
+</div>
 </body>
 </html>

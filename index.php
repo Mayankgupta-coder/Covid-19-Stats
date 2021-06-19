e3fc71fa489946acf4e45c873d7ccf87bcb9a9b5
Mayankgupta-coder <mynkgpt16@gmail.com>
2021-06-19 20:01:27 +0530

    improve css

--- index.php
+++ index.php
@@ -24,13 +24,14 @@ $len=count($content_arr['cases_time_series']);
         }
         .container-fluid
         {
-            height:200px;
+            height:180px;
             width:250px;
             border:1px solid black;
             box-shadow: 1px 1px 2px 2px grey;
-            margin-bottom:8px;
+            margin-bottom:15px;
             text-align:Center;
-            padding:40px 0px;
+            padding:30px 0px;
+            border-radius:10px;
         }
         #div
         {
@@ -49,63 +50,90 @@ $len=count($content_arr['cases_time_series']);
         font-family: 'Crimson Pro', serif;
         font-family: 'Khand', sans-serif;
         color:red;
-        font-size:25px;
+        font-size:30px;
       }
       .data2
       {
         font-family: 'Crimson Pro', serif;
         font-family: 'Khand', sans-serif;
-        color:blue;
-        font-size:25px;
+        color:green;
+        font-size:30px;
       }
       .data3
       {
         font-family: 'Crimson Pro', serif;
         font-family: 'Khand', sans-serif;
-        color:green;
-        font-size:25px;
+        color:Grey;
+        font-size:30px;
       }
+      .data4
+      {
+        font-family: 'Crimson Pro', serif;
+        font-family: 'Khand', sans-serif;
+        color:blue;
+        font-size:30px;
+      }
+      .container-fluid:hover {
+        transform: scale(1.1);
+        cursor: pointer;
+}
         </style>
 </head>
 <body>
 <?php
-$confirm = $content_arr['cases_time_series'][$len-1]['totalconfirmed'];
+$confirm = $content_arr['statewise'][0]['confirmed'];
 setlocale(LC_MONETARY, 'en_IN');
 $confirm = money_format('%!i', $confirm);
 
 
-$recover= $content_arr['cases_time_series'][$len-1]['totalrecovered'];
+$recover= $content_arr['statewise'][0]['recovered'];
 setlocale(LC_MONETARY, 'en_IN');
 $recover = money_format('%!i', $recover);
 
 
-$deceased = $content_arr['cases_time_series'][$len-1]['totaldeceased'];
+$deceased = $content_arr['statewise'][0]['deaths'];
 setlocale(LC_MONETARY, 'en_IN');
 $deceased = money_format('%!i', $deceased);
+
+
+$active = $content_arr['statewise'][0]['active'];
+setlocale(LC_MONETARY, 'en_IN');
+$active = money_format('%!i', $active);
 
 ?>
 <br/><br/>
 <div class="text">Covid-19 Data</div>
 <br/><br/>
 <div id="div">
+
 <div class="container-fluid">
-<b class="data1"> CONFIRMED CASES</b>
+<b class="data1"> CONFIRMED </b>
 <br/>
 <br/>
 <b class="data1"><?php echo $confirm[0] ?></b>
 </div>
+
 <div class="container-fluid">
-<b class="data2">RECOVERED CASES</b>
+<b class="data4">Active</b>
+<br/>
+<br/>
+<b class="data4"><?php echo $active[0]?></b>
+</div>
+
+<div class="container-fluid">
+<b class="data2">RECOVERED</b>
 <br/>
 <br/>
 <b class="data2"><?php echo $recover[0] ?></b>
 </div>
+
 <div class="container-fluid">
-<b class="data3">DECEASED CASES</b>
+<b class="data3">DECEASED</b>
 <br/>
 <br/>
 <b class="data3"><?php echo $deceased[0]?></b>
 </div>
+
     </div>
   
 </body

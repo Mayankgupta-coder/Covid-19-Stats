83151031d4ebe1c5b727e1bc33a546d0102acf6d
Mayankgupta-coder <mynkgpt16@gmail.com>
2021-09-08 17:12:08 +0530

    minor changes made

--- graphs.php
+++ graphs.php
@@ -6,11 +6,6 @@ $content=file_get_contents('https://api.covid19india.org/data.json');
 $content_arr=json_decode($content,true);
 $len=count($content_arr['cases_time_series']);
 $len1=count($content_arr['tested']);
-
-$content1=file_get_contents('https://indiancovid-19.herokuapp.com/covid_data.json');
-$content_arr1=json_decode($content1,true);
-$l=count($content_arr1);
-
 ?>
 
 <html>
@@ -120,15 +115,11 @@ google.charts.load('current', {'packages':['corechart']});
       function drawChart3() {
         var data3 = google.visualization.arrayToDataTable([
           ['Cases', ''],
-          <?php for($i=3;$i>=1;$i--)
+          <?php for($i=12;$i>=1;$i--)
           {
-              $date=$content_arr1[$l-$i]['date'];
-              echo $date;
-              echo'<br/>';
-              echo $content_arr1[$l-$i]['confirm_cases'];
-              echo'<br/>';
+              $date=$content_arr['cases_time_series'][$len-$i]['date'];
               ?>
-          ['<?php echo $date?>',  <?php echo $content_arr1[$l-$i]['confirm_cases']?>],
+          ['<?php echo $date?>',  <?php echo $content_arr['cases_time_series'][$len-$i]['totalconfirmed']?>],
           
           <?php
           }

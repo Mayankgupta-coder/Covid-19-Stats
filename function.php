f130cbf0cb7b3d5e1e812c011a87e4e64f738e90
Mayankgupta-coder <mynkgpt16@gmail.com>
2021-09-10 22:57:45 +0530

    minor changes made

--- function.php
+++ function.php
@@ -85,4 +85,23 @@ function money_format($format, $number)
     }
     return explode(".",$format);
 }
+
+
+
+
+function bd_nice_number($n) {
+    // first strip any formatting;
+    $n = (0+str_replace(",","",$n));
+   
+    // is this a number?
+    if(!is_numeric($n)) return false;
+   
+    // now filter it;
+    if($n>1000000000000) return round(($n/1000000000000),1).' trillion';
+    else if($n>1000000000) return round(($n/1000000000),1).' billion';
+    else if($n>1000000) return round(($n/1000000),1).' million';
+    else if($n>1000) return round(($n/1000),1).' thousand';
+   
+    return number_format($n);
+}
 ?>

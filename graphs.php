9d4da7fd507ad2aca66e6dae5bd8cf6d53791b36
Mayankgupta-coder <mynkgpt16@gmail.com>
2021-09-13 23:00:08 +0530

    minor changes made

--- graphs.php
+++ graphs.php
@@ -306,26 +306,18 @@ google.charts.load('current', {'packages':['corechart']});
     function drawChart7() {
 
       var data7 = new google.visualization.DataTable();
-      data7.addColumn('number', 'cases');
       data7.addColumn('number', '');
-      data7.addColumn('number', 'The Avengers');
-      data7.addColumn('number', 'Transformers: Age of Extinction');
-
+      data7.addColumn('number', 'cases');
+     
       data7.addRows([
-        [1,  37.8, 80.8, 41.8],
-        [2,  30.9, 69.5, 32.4],
-        [3,  25.4,   57, 25.7],
-        [4,  11.7, 18.8, 10.5],
-        [5,  11.9, 17.6, 10.4],
-        [6,   8.8, 13.6,  7.7],
-        [7,   7.6, 12.3,  9.6],
-        [8,  12.3, 29.2, 10.6],
-        [9,  16.9, 42.9, 14.8],
-        [10, 12.8, 30.9, 11.6],
-        [11,  5.3,  7.9,  4.7],
-        [12,  6.6,  8.4,  5.2],
-        [13,  4.8,  6.3,  3.6],
-        [14,  4.2,  6.2,  3.4]
+        <?php for($i=8;$i>=1;$i--)
+          {
+              $date=$content_arr1[$l-$i]['date'];
+              ?>
+        ['<?php echo $date?>',  <?php echo $content_arr1[$l-$i]['temp_deceased']?>],
+        <?php
+          }
+          ?>
       ]);
 
       var options7 = {
